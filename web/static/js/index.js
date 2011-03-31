fa057d8bcf141a40d457fc4214fafc9c33dd0de5
braddunbar <dunbarb2@gmail.com>
2011-03-30 22:35:10 -0400

    select nav with scroll; copy

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,3 +1,60 @@
 jQuery(function($){
 
+  var Section = Backbone.View.extend({
+
+    initialize: function(options){
+      this._a = options.a;
+    },
+
+    select: function(){
+      this._a.addClass('active');
+    },
+
+    deselect: function(){
+      this._a.removeClass('active');
+    }
+
+  });
+
+  var App = Backbone.View.extend({
+
+    initialize: function(){
+      var ul = $('nav ul');
+
+      this.sections = $('section').map(function(){
+        var el = $(this);
+        return new Section({
+          el: el,
+          a: ul.find('a[href=#' + el.attr('id') + ']')
+        });
+      });
+
+      this.update();
+    },
+
+    events: {
+      'scroll': 'update'
+    },
+
+    update: _.debounce(function(){
+      var top = this.el.scrollTop(), sections = this.sections;
+
+      console.log(top, _.pluck(sections, 'top'));
+
+      if(this.active) this.active.deselect();
+
+      for(var i = 0, l = sections.length; i < l; i++){
+        if(sections[i].el.offset().top > top){
+          this.active = sections[i-1];
+          break;
+        }
+      }
+
+      if(this.active) this.active.select()
+    }, 50)
+
+  });
+
+  var app = new App({ el: $(window) });
+
 });
