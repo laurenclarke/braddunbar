bbe41b81f836a7e23a8505804369ae9cb22f13b7
braddunbar <dunbarb2@gmail.com>
2011-04-01 07:41:28 -0400

    favicon

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 tup upd && \
-rsync -f "- *.scss" -f "- Tupfile" -f "- *.swp" --delete --delete-excluded -avz -e ssh web/ brad@braddunbar.net:/var/sites/braddunbar
+rsync \
+	-f "- *.scss" \
+	-f "- static/css/compass" \
+	-f "- Tupfile" \
+	-f "- *.swp" \
+	--delete --delete-excluded \
+	-Lavz -e ssh web/ brad@braddunbar.net:/var/sites/braddunbar
