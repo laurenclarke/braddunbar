4030596c6bc18e971f7cc0f1e3acfeac6ba4c663
braddunbar <dunbarb2@gmail.com>
2011-04-06 07:27:55 -0400

    start/stop site on deploy; need space before @charset

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 tup upd && \
+ssh -t brad@braddunbar.net sudo stop braddunbar && \
 rsync \
 	-f "- *.scss" \
 	-f "- static/css/compass" \
 	-f "- Tupfile" \
 	-f "- *.swp" \
+	-f "- *.swo" \
 	-f "- .gitignore" \
 	--delete --delete-excluded \
-	-Lavz -e ssh web/ brad@braddunbar.net:/var/sites/braddunbar
+	-Lavz -e ssh web/ brad@braddunbar.net:/var/sites/braddunbar && \
+ssh -t brad@braddunbar.net sudo start braddunbar
