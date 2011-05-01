bdcb8a8b56b0730c12bc7e44cddbe616472ca286
braddunbar <dunbarb2@gmail.com>
2011-05-01 19:17:31 -0400

    deploy with modules

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 git submodule update --init && \
 if [ ! -d .tup ] ; then tup init; fi && \
 tup upd && \
-ssh -t brad@braddunbar.net sudo stop braddunbar && \
+ssh -t brad@braddunbar.net sudo stop braddunbar || \
 rsync \
 	-f "- *.scss" \
 	-f "- static/css/compass" \
@@ -10,6 +10,9 @@ rsync \
 	-f "- *.swp" \
 	-f "- *.swo" \
 	-f "- .gitignore" \
+	-f "- node_modules" \
+	-f "P node_modules" \
 	--delete --delete-excluded \
 	-Lavz -e ssh web/ brad@braddunbar.net:/var/sites/braddunbar && \
+ssh -t brad@braddunbar.net "cd /var/sites/braddunbar && npm install" && \
 ssh -t brad@braddunbar.net sudo start braddunbar
