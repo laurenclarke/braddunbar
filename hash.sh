55b9c944f34f93db06668ad1a4f1ee4b0e7fbec3
braddunbar <dunbarb2@gmail.com>
2011-05-14 18:33:46 -0400

    coffee

--- hash.sh
+++ hash.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-echo "module.exports = {"
+echo "module.exports ="
 
 for f in $*
 do
-	echo "\"/$f\": \"/$f?`git hash-object $f`\","
+	echo "  '/$f': '/$f?`git hash-object $f`'"
 done
-
-echo "};"
